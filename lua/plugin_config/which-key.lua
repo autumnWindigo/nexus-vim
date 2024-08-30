23c6315143251872de11655d8ee0d1c0b57b075b
autumnWindigo <autumn@windigo.xyz>
2024-08-29 20:17:12 -0400

    Updated which key

--- lua/plugin_config/which-key.lua
+++ lua/plugin_config/which-key.lua
@@ -2,56 +2,39 @@ local wk = require("which-key")
 local fterm = require('FTerm')
 local dap = require('dap')
 
+wk.setup()
 
-wk.register({
-    f = {
-        name = "file", -- optional group name
-        f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
-        r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File"}, -- additional options for creating the keymap
-        b = { "<cmd>Telescope file_browser<cr>", "Browser"},
-    },
-    t = {
-        name = "toggle",
-        t = { "<cmd>NvimTreeToggle<cr>", "Nvim-tree" },
-        g = { "<cmd>Neogit<cr>", "Neogit" },
-    },
-    [";"] = {
-        name = "floaterm",
-        b = { function() fterm.scratch({cmd = 'bacon'}) end, "bacon" } ,
-        g = { function() fterm.scratch({cmd = 'lazygit'}) end, "lazygit" },
-        t = { function() fterm.scratch({cmd = 'btop'}) end, "btop"},
-    },
-    c = {
-        name = "code",
-        d = {
-            name = "debug",
-            b = { function() dap.toggle_breakpoint() end, "Breakpoint" },
-            c = { function() dap.continue() end, "Continue" },
-        },
-        p = {
-            name = "cpp",
-            b = { "<cmd>CMakeBuild<cr>", "Build" },
-            r = { "<cmd>CMakeRun<cr>", "Run" },
-            d = { "<cmd>CMakeDebug<cr>", "Debug"},
-            ---@diagnostic disable-next-line: missing-fields
-            c = { function() fterm.scratch({cmd = 'clang-tidy --config-file=.clang-tidy -p build ' .. vim.fn.expand('%')}) end, "Check"},
-            f = { "<cmd>!clang-format " .. vim.fn.expand('%') .. " --style=google --sort-includes -i<cr><cr>", "Format" },
-        },
-    },
-    b = {
-        name = "buffers",
-        i = { "<cmd>Telescope buffers<cr>", "interactive" },
-    },
-    l = {
-        name = "lsp",
-        a = { function() vim.lsp.buf.code_action() end, "action"},
-        f = { function() vim.lsp.buf.format() end, "format"},
-    },
-    v = {
-        name = "variable",
-        d = { function() vim.lsp.buf.definition() end, "definition"},
-        r = { function() vim.lsp.buf.references() end, "references"},
-        n = { function() vim.lsp.buf.rename() end, "rename" },
-        h = { function() vim.lsp.buf.hover() end, "hover" },
-    },
-}, { prefix = "<leader>" })
+wk.add({
+    { "<leader>f",   group = "file" }, -- file group
+    { "<leader>ff",  "<cmd>Telescope find_files<cr>",                   desc = "Find File", },
+    { "<leader>fb",  "<cmd>telescope file_browser<cr>",                 desc = "File Browser", },
+    { "<leader>fr",  "<cmd>Telescope oldfiles<cr>",                     desc = "Open Recent", },
+
+    { "<leader>t",   group = "toggle", },
+    { "<leader>tt",  "<cmd>NvimTreeToggle<cr>",                         desc = "NeoTree", },
+
+    { "<leader>g",   group = "git", },
+    { "<leader>gn",  "<cmd>Neogit<cr>",                                 desc = "Neogit", },
+    ---@diagnostic disable-next-line: missing-fields
+    { "<leader>gl",  function() fterm.scratch({ cmd = 'lazygit' }) end, desc = "Lazy Git" },
+
+    { "<leader>c",   group = "code" },
+    { "<leader>cd",  group = "debug" },
+    { "<leader>cdb", function() dap.toggle_breakpoint() end,            desc = "Breakpoint" },
+    { "<leader>cdc", function() dap.continue() end,                     desc = "Continue" },
+
+    { "<leader>cp",  group = "cpp" },
+    { "<leader>cpb", "<cmd>CMakeBuild<cr>",                             desc = "Build" },
+    { "<leader>cpd", "<cmd>CMakeDebug<cr>",                             desc = "Debug" },
+    { "<leader>cpr", "<cmd>CMakeRun<cr>",                               desc = "Run" },
+
+    { "<leader>l",   group = "lsp" },
+    { "<leader>la",  function() vim.lsp.buf.code_action() end,          desc = "action" },
+    { "<leader>lf",  function() vim.lsp.buf.format() end,               desc = "format" },
+
+    { "<leader>v",   group = "variable" },
+    { "<leader>vd",  function() vim.lsp.buf.definition() end,           desc = "definition" },
+    { "<leader>vh",  function() vim.lsp.buf.references() end,           desc = "references" },
+    { "<leader>vn",  function() vim.lsp.buf.rename() end,               desc = "rename" },
+    { "<leader>vr",  function() vim.lsp.buf.hover() end,                desc = "hover" },
+})
